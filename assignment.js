4c86f66e3473b50a567469023535b6f683936bc1
Kavya p.s <107993078+Kavya1807@users.noreply.github.com>
2023-08-18 17:46:49 +0530

    commit

---
+++ assignment.js
@@ -0,0 +1,12 @@
+// Create a function that takes two numbers as parameters and returns their sum.
+
+function addSum(a,b)
+{
+   
+    console.log("The Sum of Two Numbers are: "+(a+b));
+
+}
+
+addSum(100,20)
+
+addSum(400,400)
