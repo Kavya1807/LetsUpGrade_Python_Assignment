bf8681e2d4a2fb98fc22714dac7c87394852d62f
Kavya P.S <kavyahemsun@gmail.com>
2023-12-18 19:02:44 +0530

    changed

--- assignment.js
+++ assignment.js
@@ -1,8 +1,8 @@
 // Create a function that takes two numbers as parameters and returns their sum.
 
-function addSum(m,n)
+function addSum(a,b)
 {
-    console.log("The Sum of Two Numbers are: "+(m+n));
+    console.log("The Sum of Two Numbers are: "+(a+b));
 
 }
 
